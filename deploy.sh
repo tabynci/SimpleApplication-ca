88a381d82c6328594de65064db77c5134eb6995a
tabynci <100541229+tabynci@users.noreply.github.com>
2022-07-08 12:04:51 +0100

    Updated pipelines to be secured with https

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,7 @@ pm2 stop simple_app
 cd SimpleApplication-ca/
 # Install application dependencies
 npm install
-# Start the application with the process name example_app using pm2
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+# Start the application with the process name simple_app using pm2
 pm2 start ./bin/www --name simple_app
